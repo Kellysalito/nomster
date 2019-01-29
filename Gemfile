2d114bb566a669d3bbe512c8367c420172b71dda
Kresmlin <kycmmas@gmail.com>
2019-01-29 08:04:07 +0000

    pagination

--- Gemfile
+++ Gemfile
@@ -69,4 +69,5 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
-gem 'paginate', '~> 4.0', '>= 4.0.1'
+gem 'will_paginate', '~> 3.1.0'
+gem 'will_paginate-bootstrap'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
   def index 
-    @places = Place.all.paginate(:page => params[:page], :per_page => 10) 
+    @places = Place.paginate(:page => params[:page], :per_page => 10) 
   end  
+  
 end
