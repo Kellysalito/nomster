63ee2efd93978a33867c7599cb3b9533971e466e
Kresmlin <kycmmas@gmail.com>
2019-02-16 19:16:55 +0000

    errors and more errors

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+# end
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -30,7 +31,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fit: [50, 50]
+  process resize_to_fit: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
@@ -44,4 +45,5 @@ class PictureUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
 
